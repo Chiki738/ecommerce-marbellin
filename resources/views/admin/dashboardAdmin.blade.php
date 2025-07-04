ac7ab46e8fff4799c66b6dfd724742c470992a1f
Chiki738 <carlosdalzamorah@gmail.com>
2025-07-04 15:57:45 -0500

    refactor: Mejorar la estructura del código en dashboardAdmin.blade.php al mover estilos al encabezado

--- resources/views/admin/dashboardAdmin.blade.php
+++ resources/views/admin/dashboardAdmin.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<iframe
-    src="http://127.0.0.1:8050">
-</iframe>
+<style>
+    html,
+    body {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+    }
+
+    iframe {
+        width: 100%;
+        height: 100vh;
+        border: none;
+        display: block;
+    }
+</style>
+
+<iframe src="http://127.0.0.1:8050"></iframe>
 @endsection
