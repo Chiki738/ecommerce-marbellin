8b3f8d06187eb93c71b53f4efd1f150277108006
Chiki738 <carlosdalzamorah@gmail.com>
2025-07-13 14:56:28 -0500

    fix: Corregir la verificación del entorno de producción en AppServiceProvider para forzar HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');  // Forzar HTTPS en producción
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
         }
     }
 }
